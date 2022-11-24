7724cb50d25b0eb39bf70fbd703ae88a8494fb94
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2022-11-24 08:06:51 +0300

    completd assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if(distance > 42){
+        return distance - 42;
+    }else {
+        return 42 - distance;
+    }
+    
+}
+function distanceFromHqInFeet(distance) {
+
+       return distanceFromHqInBlocks(distance)*264
+    
+ }
+ function distanceTravelledInFeet(start, destination){
+    if (start > destination) {
+        return(start - destination)*264
+    }else if (start < destination) {
+        return(destination - start)*264
+    }
+ 
+ }
+ function calculatesFarePrice(start, destination){
+    let feet =distanceTravelledInFeet(start, destination)
+    if(feet<400){
+        return 0;
+    }else if (feet>400 && feet<2000){
+        return (feet - 400)*0.02
+    }else if (feet>2000 && feet<2500){
+        return 25;
+    }else{
+        return "cannot travel that far";
+    }
+ }
